16c6ddf25fca925ef06a536fc2eb30e93607e84c
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-04-28 13:19:17 -0300

    update: created component Menu

---
+++ src/components/Home/Menu.jsx
@@ -0,0 +1,25 @@
+import { Home, ShoppingCart, Heart, ClipboardList } from "lucide-react";
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <div className="fixed bottom-0 ">
+      <div className="w-[393px] h-[61px] flex items-center justify-around bg-redPrimary rounded-[px]">
+        <Link href="#">
+          <Home className="text-white" size={30} />
+        </Link>
+        <Link href="#">
+          <ShoppingCart className="text-white" size={30} />
+        </Link>
+        <Link href="#">
+          <Heart className="text-white" size={30} />
+        </Link>
+        <Link href="/Pedidos">
+          <ClipboardList className="text-white" size={30} />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
