cb5d3b4c437e61d65aefe8627e1ce7323e4f6210
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-04-28 13:17:02 -0300

    Update: alterated class for tags jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,26 @@
-"use client"
-
 import Buttons from "@/components/Home/Buttons";
 import CardsProducts from "@/components/Home/CardsProducts";
 import Header from "@/components/Home/Header";
+import Menu from "@/components/Home/Menu";
 import SearchItens from "@/components/Home/SearchItens";
 
 export default function Home() {
-
   return (
-    <div className="w-[393px] min-h-screen flex flex-col px-[33px] py-[37px] bg-[#F5F5F5]">
-      <Header />
-      <SearchItens />
-      <Buttons />
-      <div className="pt-[36px]">
-        <p className="font-semibold text-[18px] text-blackPrimary">Mais procurados</p>
+    <div className="w-full flex justify-center items-center bg-[#F5F5F5] overflow-x-hidden">
+      <div>
+        <div className="w-[393px] min-h-screen flex flex-col px-[33px] pt-[37px]">
+          <Header />
+          <SearchItens />
+          <Buttons />
+          <div className="pt-[36px]">
+            <p className="font-semibold text-[18px] text-blackPrimary">
+              Mais procurados
+            </p>
+          </div>
+          <CardsProducts />
+        </div>
+        <Menu />
       </div>
-      <CardsProducts />
     </div>
   );
 }
