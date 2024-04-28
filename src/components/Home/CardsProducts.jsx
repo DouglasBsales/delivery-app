de3ea245caa981ebce457af7f6728cbc47d99595
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-04-28 13:19:06 -0300

    update: created component cardsProducts

--- src/components/Home/CardsProducts.jsx
+++ src/components/Home/CardsProducts.jsx
@@ -5,7 +5,7 @@ import { Heart } from "lucide-react";
 
 const CardsProducts = () => {
   return (
-    <div className="pt-[27px]">
+    <div className="flex flex-col gap-5">
       <Link
         href="#"
         className="w-[326px] h-[146px] flex flex-col bg-white rounded-[8px] pl-[16px] relative"
@@ -18,7 +18,34 @@ const CardsProducts = () => {
           </div>
           <div className="w-[160px] pt-[21px]">
             <p className="text-xs text-blackOpacity font-semibold">Burguer</p>
-            <p className="text-[18px] text-blackPrimary font-semibold relative top-[-8px]">
+            <p className="text-[18px] text-blackPrimary font-semibold relative top-[-3px]">
+              Hamburguer
+            </p>
+            <p className="text-[10px] font-semibold text-blackOpacity relative top-[-8px]">
+              Um hambúrguer simples com carne suculenta grelhada, queijo
+              derretido, alface ...
+            </p>
+            <p className="text-redPrimary font-bold text-[18px]">R$ 12,00</p>
+          </div>
+          <div>
+            <Image src={Burguer} alt="" width={134} height={135} />
+          </div>
+        </div>
+      </Link>
+
+      <Link
+        href="#"
+        className="w-[326px] h-[146px] flex flex-col bg-white rounded-[8px] pl-[16px] relative"
+      >
+        <div className="flex justify-between">
+          <div className="w-[305px] justify-end flex absolute pt-[5px]">
+            <button>
+              <Heart className={"text-redPrimary"} />
+            </button>
+          </div>
+          <div className="w-[160px] pt-[21px]">
+            <p className="text-xs text-blackOpacity font-semibold">Burguer</p>
+            <p className="text-[18px] text-blackPrimary font-semibold relative top-[-3px]">
               Hamburguer
             </p>
             <p className="text-[10px] font-semibold text-blackOpacity relative top-[-8px]">
