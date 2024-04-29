09e3899c412982632270a0170bc973356a926c7a
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-04-29 18:22:11 -0300

    upddate: modified logic of cards

--- src/components/Home/CardsProducts.jsx
+++ src/components/Home/CardsProducts.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { Heart } from "lucide-react";
 
-const CardsProducts = ({ name, category, photo, price, description, descriptionCard}) => {
+const CardsProducts = ({ name, portion, category, photo, price, descriptionCard}) => {
   return (
-    <div className="flex flex-col gap-5 pt-8 pb-20">
+    <div className="flex flex-col pt-8">
       <Link
         href="#"
-        className="w-[326px] h-[146px] flex flex-col bg-white rounded-[8px] pl-[16px] relative"
+        className="w-[326px] flex flex-col bg-white rounded-[8px] pl-[16px] relative"
       >
         <div className="flex justify-between">
           <div className="w-[305px] justify-end flex absolute pt-[5px]">
@@ -20,13 +20,14 @@ const CardsProducts = ({ name, category, photo, price, description, descriptionC
             <p className="text-[18px] text-blackPrimary font-semibold relative top-[-3px]">
               {name}
             </p>
-            <p className="text-[10px] font-semibold text-blackOpacity relative top-[-8px]">
+            <p className="text-redPrimary text-xs relative bottom-2">{portion}</p>
+            <p className="text-[10px] font-semibold text-blackOpacity">
             {descriptionCard}...
             </p>
-            <p className="text-redPrimary font-bold text-[18px]">R$ {price.toFixed(2).replace('.', ",")}</p>
+            <p className="text-redPrimary font-bold text-[18px] pb-3 pt-2">R$ {price.toFixed(2).replace('.', ",")}</p>
           </div>
-          <div>
-            <Image src={photo} alt="" width={134} height={135} />
+          <div className="flex items-center">
+            <Image src={photo} alt="hamburguer" width={134} height={135}/>
           </div>
         </div>
       </Link>
