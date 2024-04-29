3e54eaf8b2db985443252ae37c346af60aef7c94
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-04-29 18:21:41 -0300

    update: update butons and logics

--- src/components/Home/Buttons.jsx
+++ src/components/Home/Buttons.jsx
@@ -1,25 +1,48 @@
-const Buttons = () => {
+"use client";
+
+
+const Buttons = ({ filterPizza, filterTodos, filterBurguer, isSelected }) => {
   return (
     <div className="w-full flex gap-[15px] pt-[18px]">
       <div>
-        <button className="w-[100px] h-[37px] bg-redPrimary rounded-[30px]">
-            <p className="text-white font-semibold text-[14px]">Todos</p>
+        <button 
+          className={`w-[100px] h-[37px] h-[37px rounded-[30px] outline-none ${
+            isSelected === "Todos"
+              ? "bg-redPrimary text-white "
+              : "bg-white text-blackPrimary "
+          }`}
+          onClick={filterTodos}
+        >
+          <p className="font-semibold text-[14px]">Todos</p>
         </button>
       </div>
 
       <div>
-        <button className="w-[100px] h-[37px] bg-white rounded-[30px]">
-            <p className="text-blackPrimary font-bold text-[14px]">Burguer</p>
+        <button
+          className={`w-[100px] h-[37px] h-[37px rounded-[30px] outline-none ${
+            isSelected === "Burguer"
+              ? "bg-redPrimary text-white "
+              : "bg-white text-blackPrimary "
+          }`}
+          onClick={filterBurguer}
+        >
+          <p className="font-bold text-[14px]">Burguer</p>
         </button>
       </div>
       <div>
-
-        <button className="w-[100px] h-[37px] bg-white rounded-[30px]">
-            <p className="text-blackPrimary font-semibold text-[14px]">Pizza</p>
+        <button
+          className={`w-[100px] h-[37px] h-[37px rounded-[30px] outline-none ${
+          isSelected === "Pizza"
+              ? "bg-redPrimary text-white "
+              : "bg-white text-blackPrimary "
+          }`}
+          onClick={filterPizza}
+        >
+          <p className="font-semibold text-[14px]">Pizza</p>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
