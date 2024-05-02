3ac9dba5a45976c880e60129f187a3cadf61c5a1
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-02 14:22:01 -0300

    updat: added props and logics

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,5 +1,9 @@
 import { ChevronLeft, Minus, Plus } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProductPage = ({
   setIsHome,
@@ -10,11 +14,54 @@ const ProductPage = ({
   price,
   description,
   portion,
+  arrayCarrinho,
+  setArrayCarrinho,
 }) => {
   const returnPageHome = () => {
     setIsHome(true);
     setIsPageProduct(false);
   };
+
+  const [countItem, setCountItem] = useState(1);
+
+  const handleClickAddItem = () => {
+    setCountItem((prevNumber) => prevNumber + 1);
+  };
+
+  const handleClickMinusItem = () => {
+    if (countItem === 1) {
+      return;
+    } else {
+      setCountItem((prevNumber) => prevNumber - 1);
+    }
+  };
+
+  const handleAddToCart = () => {
+    const newItems = Array.from({ length: countItem }, () => ({
+      photo,
+      name,
+      category,
+      price,
+      description,
+      portion,
+    }));
+    setArrayCarrinho([...arrayCarrinho, ...newItems]);
+  };
+
+  const notify = () =>
+    toast.success("Produto adicionado com sucesso", {
+      position: "top-right",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+
+  console.log(arrayCarrinho);
 
   return (
     <div className="flex flex-col min-h-screen bg-[#F5F5F5] pb-4">
@@ -36,7 +83,9 @@ const ProductPage = ({
       <div className="flex-1 max-w-[393px] px-[33px]">
         <div className="flex flex-col items-start h-full">
           <div className="w-full flex justify-between pt-[100px]">
-            <p className="w-[200px] text-xl font-bold text-blackPrimary">{name}</p>
+            <p className="w-[200px] text-xl font-bold text-blackPrimary">
+              {name}
+            </p>
             <p className="text-xl font-bold text-redPrimary">
               {" "}
               R$ {price.toFixed(2).replace(".", ",")}
@@ -61,20 +110,44 @@ const ProductPage = ({
           <div className="flex justify-between pt-[70px] gap-3">
             <div className="w-[148px] h-[61px] flex items-center justify-center rounded-[30px] bg-white">
               <div className="w-[125px] flex justify-between">
-                <button className="w-[41px] h-[41px] flex items-center justify-center bg-redPrimary rounded-full">
-                  <Plus className="text-white" />
+                <button
+                  className="w-[41px] h-[41px] flex items-center justify-center bg-redPrimary rounded-full"
+                  onClick={handleClickMinusItem}
+                >
+                  <Minus className="text-white" />
                 </button>
                 <div className="flex items-center">
-                  <p className="font-semibold text-lg">1</p>
+                  <p className="font-semibold text-lg">{countItem}</p>
                 </div>
-                <button className="w-[41px] h-[41px] flex items-center justify-center bg-redPrimary rounded-full">
-                  <Minus className="text-white"/>
+                <button
+                  className="w-[41px] h-[41px] flex items-center justify-center bg-redPrimary rounded-full"
+                  onClick={handleClickAddItem}
+                >
+                  <Plus className="text-white" />
                 </button>
               </div>
             </div>
-            <button className="w-[167px] h-[61px] flex items-center justify-center bg-redPrimary rounded-[30px]">
-              <p className="text-white text-xs font-semibold">Adicionar ao carrinho</p>
+            <button
+              className="w-[167px] h-[61px] flex items-center justify-center bg-redPrimary rounded-[30px]"
+              onClick={notify}
+            >
+              <p className="text-white text-xs font-semibold">
+                Adicionar ao carrinho
+              </p>
             </button>
+            <ToastContainer
+              position="top-right"
+              autoClose={120}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              transition={Bounce}
+            />
           </div>
         </div>
       </div>
