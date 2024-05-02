7ea2261fd592ac8d5baf47b4cf291f80d45196af
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-02 19:03:11 -0300

    update: alterated button for listaCarrinho

--- src/components/Carrinho/LIstaCarrinho.jsx
+++ src/components/Carrinho/LIstaCarrinho.jsx
@@ -57,50 +57,52 @@ const ListaCarrinho = ({ item, arrayCarrinho, setArrayCarrinho }) => {
   };
 
   return (
-    <div className="flex justify-between">
+    <div className="w-full flex justify-between">
       <div className="w-[326px] pt-[21px] text-start">
         <div className="flex items-center ">
           <div>
             <Image src={item.photo} alt="item" width={95} height={95} />
           </div>
-          <div className="flex flex-col">
-            <div className="flex gap-[6px]">
-              <p className="text-xs font-semibold text-blackOpacity">
-                {item.quantity}x
-              </p>
-              <div>
+          <div className="w-full flex justify-between">
+            <div className="flex flex-col">
+              <div className="flex gap-[6px]">
                 <p className="text-xs font-semibold text-blackOpacity">
-                  {item.name}
+                  {item.quantity}x
                 </p>
-              </div>
-              <div className="pl-[36px]">
-                <div className="w-[79px] h-[28px] bg-redPrimary rounded-md">
-                  <div className="flex justify-between p-[7px]">
-                    {countItem === 1 && (
-                      <button onClick={handleRemoveItem}>
-                        <Trash2 className="text-white" size={16} />
-                      </button>
-                    )}
-                    {countItem > 1 && (
-                      <button onClick={handleClickMinusItem}>
-                        <Minus className="text-white" size={16} />
-                      </button>
-                    )}
-                    <div className="text-xs font-semibold text-white">
-                      {countItem}
-                    </div>
-                    <button onClick={handleClickAddItem}>
-                      <Plus className="text-white" size={16} />
-                    </button>
-                  </div>
+                <div>
+                  <p className="text-xs font-semibold text-blackOpacity">
+                    {item.name}
+                  </p>
                 </div>
               </div>
+              <div>
+                <p className="text-xs text-redPrimary font-semibold">
+                  R$ {(item.price * item.quantity).toFixed(2).replace(".", ",")}
+                </p>
+              </div>
             </div>
-            <div>
-              <p className="text-xs text-redPrimary font-semibold">
-                R$ {(item.price * item.quantity).toFixed(2).replace(".", ",")}
-              </p>
-            </div>
+            <div >
+                  <div className="w-[79px] h-[28px] bg-redPrimary rounded-md">
+                    <div className="flex justify-between p-[7px]">
+                      {countItem === 1 && (
+                        <button onClick={handleRemoveItem}>
+                          <Trash2 className="text-white" size={16} />
+                        </button>
+                      )}
+                      {countItem > 1 && (
+                        <button onClick={handleClickMinusItem}>
+                          <Minus className="text-white" size={16} />
+                        </button>
+                      )}
+                      <div className="text-xs font-semibold text-white">
+                        {countItem}
+                      </div>
+                      <button onClick={handleClickAddItem}>
+                        <Plus className="text-white" size={16} />
+                      </button>
+                    </div>
+                  </div>
+                </div>
           </div>
         </div>
       </div>
