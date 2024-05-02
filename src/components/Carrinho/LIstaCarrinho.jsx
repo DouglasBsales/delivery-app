85bc1fd11b8b80c14d69f973a83e5dc0b8ac3b3f
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-02 18:12:34 -0300

    update: update listaCarrinho for the best experience user

--- src/components/Carrinho/LIstaCarrinho.jsx
+++ src/components/Carrinho/LIstaCarrinho.jsx
@@ -1,7 +1,61 @@
+import { Minus, Plus, Trash2 } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
-const ListaCarrinho = ({ item }) => {
+const ListaCarrinho = ({ item, arrayCarrinho, setArrayCarrinho }) => {
+  const [countItem, setCountItem] = useState(item.quantity);
+
+  const handleClickAddItem = () => {
+    const countItemAdd = countItem + 1;
+    setCountItem(countItemAdd);
+    handleUpdateCart(countItemAdd);
+  };
+
+  const handleClickMinusItem = () => {
+    if (countItem === 1) {
+      return;
+    } else {
+      const countItemMinus = countItem - 1;
+      setCountItem(countItemMinus);
+      handleUpdateCart(countItemMinus);
+    }
+  };
+
+  const handleRemoveItem = () => {
+    const newArrayCarrinho = arrayCarrinho.filter(
+      (cartItem) => cartItem.name !== item.name
+    );
+    setArrayCarrinho(newArrayCarrinho);
+  };
+
+  const handleUpdateCart = (updatedCount) => {
+    const existingItemIndex = arrayCarrinho.findIndex(
+      (cartItem) => cartItem.name === item.name
+    );
+
+    if (existingItemIndex !== -1) {
+      const newArrayCarrinho = [...arrayCarrinho];
+      newArrayCarrinho[existingItemIndex] = {
+        ...newArrayCarrinho[existingItemIndex],
+        quantity: updatedCount,
+      };
+      setArrayCarrinho(newArrayCarrinho);
+    } else {
+      setArrayCarrinho([
+        ...arrayCarrinho,
+        {
+          photo: item.photo,
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          description: item.description,
+          portion: item.portion,
+          quantity: updatedCount,
+        },
+      ]);
+    }
+  };
+
   return (
     <div className="flex justify-between">
       <div className="w-[326px] pt-[21px] text-start">
@@ -12,11 +66,35 @@ const ListaCarrinho = ({ item }) => {
           <div className="flex flex-col">
             <div className="flex gap-[6px]">
               <p className="text-xs font-semibold text-blackOpacity">
-                {item.quantity}
+                {item.quantity}x
               </p>
-              <p className="text-xs font-semibold text-blackOpacity">
-                {item.name}
-              </p>
+              <div>
+                <p className="text-xs font-semibold text-blackOpacity">
+                  {item.name}
+                </p>
+              </div>
+              <div className="pl-[36px]">
+                <div className="w-[79px] h-[28px] bg-redPrimary rounded-md">
+                  <div className="flex justify-between p-[7px]">
+                    {countItem === 1 && (
+                      <button onClick={handleRemoveItem}>
+                        <Trash2 className="text-white" size={16} />
+                      </button>
+                    )}
+                    {countItem > 1 && (
+                      <button onClick={handleClickMinusItem}>
+                        <Minus className="text-white" size={16} />
+                      </button>
+                    )}
+                    <div className="text-xs font-semibold text-white">
+                      {countItem}
+                    </div>
+                    <button onClick={handleClickAddItem}>
+                      <Plus className="text-white" size={16} />
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
             <div>
               <p className="text-xs text-redPrimary font-semibold">
@@ -26,7 +104,6 @@ const ListaCarrinho = ({ item }) => {
           </div>
         </div>
       </div>
-      <div className="flex items-center"></div>
     </div>
   );
 };
