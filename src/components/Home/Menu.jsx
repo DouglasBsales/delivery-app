dd1d637d6b9e8cf3f3612a32d4ccbdbbcf5b72ad
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-02 14:22:41 -0300

    update: update menu for alterated states

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -1,19 +1,31 @@
 import { Home, ShoppingCart, Heart, ClipboardList } from "lucide-react";
 import Link from "next/link";
 
-const Menu = () => {
+const Menu = ({ home, pageCarrinho }) => {
+
+  const pageHome = ()=>{
+    pageCarrinho(false)
+    home(true)
+  }
+
+  const pageCarrinhoProduct = ()=>{
+    pageCarrinho(true)
+    home(false)
+  }
+
+
   return (
     <div className="fixed bottom-0">
       <div className="w-[393px] h-[61px] flex items-center justify-around bg-redPrimary rounded-[px]">
-        <Link href="#">
+        <button onClick={pageHome}>
           <Home className="text-white" size={30} />
-        </Link>
-        <Link href="#">
+        </button>
+        <button onClick={pageCarrinhoProduct}>
           <ShoppingCart className="text-white" size={30} />
-        </Link>
-        <Link href="#">
+        </button>
+        <button>
           <Heart className="text-white" size={30} />
-        </Link>
+        </button>
         <Link href="/Pedidos">
           <ClipboardList className="text-white" size={30} />
         </Link>
