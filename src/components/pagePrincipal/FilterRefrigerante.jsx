0a34a8d34c95897d0f41afd0a0b819ebe2de72b1
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-05 15:48:29 -0300

    added component filter carrinho

---
+++ src/components/pagePrincipal/FilterRefrigerante.jsx
@@ -0,0 +1,31 @@
+import CardsProducts from "../Home/CardsProducts";
+
+const FilterRefrigerante = ({ items, clickPageProduct }) => {
+  return (
+    <div>
+      <div>
+        <p className="font-semibold text-[18px] text-blackPrimary pt-8">
+          Refrigerante
+        </p>
+      </div>
+      {items &&
+        items
+          .filter((product) => product.category === "refrigerante")
+          .map((product) => (
+            <div key={product.id}>
+              <CardsProducts
+                id={product.id}
+                name={product.name}
+                category={product.category}
+                photo={product.photo}
+                price={product.price}
+                descriptionCard={product.descriptionCard}
+                clickPageProduct={clickPageProduct}
+              />
+            </div>
+          ))}
+    </div>
+  );
+};
+
+export default FilterRefrigerante;
