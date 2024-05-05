7e7925844abcebca9062b71b8e622d2a017d496e
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-05 15:48:22 -0300

    added value of itens in carrinho

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -1,7 +1,7 @@
 import { Home, ShoppingCart, Heart, ClipboardList } from "lucide-react";
 import Link from "next/link";
 
-const Menu = ({ home, pageCarrinho, pagePedido }) => {
+const Menu = ({ home, pageCarrinho, pagePedido, arrayProductCarrinho }) => {
   const pageHome = () => {
     pageCarrinho(false);
     home(true);
@@ -17,15 +17,27 @@ const Menu = ({ home, pageCarrinho, pagePedido }) => {
     home(false);
   };
 
+  console.log(arrayProductCarrinho);
+
   return (
     <div className="fixed bottom-0">
-      <div className="w-[393px] h-[61px] flex items-center justify-around bg-redPrimary rounded-[px]">
+      <div className="w-[393px] h-[65px] flex items-center justify-around bg-redPrimary rounded-t-md pt-2">
         <button onClick={pageHome}>
           <Home className="text-white" size={30} />
         </button>
-        <button onClick={pageCarrinhoProduct}>
-          <ShoppingCart className="text-white" size={30} />
-        </button>
+        <div className="flex flex-col relative">
+          <button onClick={pageCarrinhoProduct}>
+          <div className="absolute top-[-12px] left-5 ">
+            <p className="font-bold text-white text-xs">
+              {arrayProductCarrinho.reduce(
+                (total, produto) => total + produto.quantity,
+                0
+              )}
+            </p>
+          </div>
+            <ShoppingCart className="text-white" size={30} />
+          </button>
+        </div>
         <button>
           <Heart className="text-white" size={30} />
         </button>
