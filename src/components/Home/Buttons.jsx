6105bfa515374ac90c88b6574b203d64e9e218ab
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-05 15:47:54 -0300

    added filter of Refrigerante

--- src/components/Home/Buttons.jsx
+++ src/components/Home/Buttons.jsx
@@ -1,12 +1,18 @@
 "use client";
+import "./Buttons.css";
 
-
-const Buttons = ({ filterPizza, filterTodos, filterBurguer, isSelected }) => {
+const Buttons = ({
+  filterPizza,
+  filterTodos,
+  filterBurguer,
+  filterRefri,
+  isSelected,
+}) => {
   return (
-    <div className="w-full flex gap-[15px] pt-[18px]">
+    <div className="w-full flex gap-[15px] pt-[18px] overflow-x-auto  container">
       <div>
-        <button 
-          className={`w-[100px] h-[37px] h-[37px rounded-[30px] outline-none ${
+        <button
+          className={`px-6 py-2  rounded-[30px] outline-none ${
             isSelected === "Todos"
               ? "bg-redPrimary text-white "
               : "bg-white text-blackPrimary "
@@ -19,7 +25,7 @@ const Buttons = ({ filterPizza, filterTodos, filterBurguer, isSelected }) => {
 
       <div>
         <button
-          className={`w-[100px] h-[37px] h-[37px rounded-[30px] outline-none ${
+          className={`px-6 py-2 rounded-[30px] outline-none ${
             isSelected === "Burguer"
               ? "bg-redPrimary text-white "
               : "bg-white text-blackPrimary "
@@ -31,14 +37,26 @@ const Buttons = ({ filterPizza, filterTodos, filterBurguer, isSelected }) => {
       </div>
       <div>
         <button
-          className={`w-[100px] h-[37px] h-[37px rounded-[30px] outline-none ${
-          isSelected === "Pizza"
+          className={`px-6 py-2  rounded-[30px] outline-none ${
+            isSelected === "Pizza"
               ? "bg-redPrimary text-white "
               : "bg-white text-blackPrimary "
           }`}
           onClick={filterPizza}
         >
           <p className="font-semibold text-[14px]">Pizza</p>
+        </button>
+      </div>
+      <div>
+        <button
+          className={`px-6 py-2 rounded-[30px] outline-none ${
+            isSelected === "Refrigerante"
+              ? "bg-redPrimary text-white "
+              : "bg-white text-blackPrimary "
+          }`}
+          onClick={filterRefri}
+        >
+          <p className="font-semibold text-[14px]">Refrigerante</p>
         </button>
       </div>
     </div>
