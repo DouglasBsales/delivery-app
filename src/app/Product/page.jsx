1afedd2d16bc0383d3ea7de51881621c83330f78
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-09 21:50:35 -0300

    alterated tags p

--- src/app/Product/page.jsx
+++ src/app/Product/page.jsx
@@ -1,6 +1,7 @@
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, Minus, Plus } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Bounce, ToastContainer } from "react-toastify";
 
 export default function Product() {
   return (
@@ -15,7 +16,7 @@ export default function Product() {
             </Link>
           </div>
           <div className="flex justify-center items-center pt-[130px]">
-            <Image src={photo} alt="item" width={300} height={300} />
+            <Image src={""} alt="item" width={300} height={300} />
           </div>
         </div>
       </div>
@@ -23,21 +24,21 @@ export default function Product() {
         <div className="flex flex-col items-start h-full">
           <div className="w-full flex justify-between pt-[100px]">
             <p className="w-[200px] text-xl font-bold text-blackPrimary">
-              {name}
+           
             </p>
             <p className="text-xl font-bold text-redPrimary">
-              R$ {price.toFixed(2).replace(".", ",")}
+ 
             </p>
           </div>
           <div>
-            <p className="text-redPrimary font-semibold">{portion}</p>
+            <p className="text-redPrimary font-semibold"></p>
           </div>
           <div>
-            <p className="text-redPrimary font-semibold">{ml}</p>
+            <p className="text-redPrimary font-semibold"></p>
           </div>
           <div>
             <p className="text-xs font-semibold text-blackOpacity">
-              {category}
+
             </p>
           </div>
           <div className="w-[100px] h-[37px] flex items-center justify-center  rounded-[30px] bg-redPrimary mt-5">
@@ -45,7 +46,7 @@ export default function Product() {
           </div>
           <div className="pt-5">
             <p className="font-semibold text-blackOpacity text-sm">
-              {description}
+
             </p>
           </div>
           <div className="flex justify-between pt-[70px] gap-3">
@@ -53,12 +54,11 @@ export default function Product() {
               <div className="w-[125px] flex justify-between">
                 <button
                   className="w-[41px] h-[41px] flex items-center justify-center bg-redPrimary rounded-full"
-                  onClick={handleClickMinusItem}
                 >
                   <Minus className="text-white" />
                 </button>
                 <div className="flex items-center">
-                  <p className="font-semibold text-lg">{countItem}</p>
+                  <p className="font-semibold text-lg"></p>
                 </div>
                 <button
                   className="w-[41px] h-[41px] flex items-center justify-center bg-redPrimary rounded-full"
