07017d80938734b6ef761cea27c86c9c3fc9f7f3
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-09 21:50:03 -0300

    comments for better viewing

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { Search, SlidersHorizontal } from "lucide-react";
-
-import { useFetch } from "@/hooks/useFetch";
-import { useFetchBest } from "@/hooks/useFetchBest";
 import { useState } from "react";
-import { FilterItensContext } from "@/Context/FilterItensContext";
+import { HomeContextProvider } from "@/Context/Home/HomeContextProvider";
 
 import Header from "@/components/Home/Header";
 import Buttons from "@/components/Home/Buttons";
@@ -18,21 +14,16 @@ import PagePedidos from "@/components/PagePedidos/PagePedidos";
 import PageFavoritos from "@/components/ItensFavoritos/PageFavoritos";
 import FilterCategory from "@/components/pagePrincipal/FilterCategory";
 import FilterItemsValue from "@/components/pagePrincipal/FilterItemsValue";
+import SearchItens from "@/components/Home/SearchItens";
 
-const url = "https://josn-delivery-app.vercel.app/products";
-const urlBest = "https://josn-delivery-app.vercel.app/BestProducts";
 
 export default function Home() {
-  const { data: items } = useFetch(url);
-  const { dados: itemsBest } = useFetchBest(urlBest);
 
   const [allItens, setAllItens] = useState(true);
-  const [isSelected, setIsSelected] = useState("Todos");
   const [filteredItems, setFilteredItems] = useState([]);
 
-  const [value, setValue] = useState("");
-
-  const [isHome, setIsHome] = useState(true);
+  {
+    /* const [isHome, setIsHome] = useState(true);
   const [isPageProduct, setIsPageProduct] = useState(false);
   const [productExib, setProductExib] = useState(null);
 
@@ -46,117 +37,38 @@ export default function Home() {
   const [arrayFavoritos, setArrayFavoritos] = useState([]);
   const [valuePayment, setValuePayment] = useState("0");
 
-  const handleClickFilterItem = () => {
-    if (value.trim() === "") {
-      alert("Insira um item, por favor");
-      return;
-    }
-
-    if (!items) {
-      return;
-    }
-
-    const filteredItemsSearch = items.filter((product) =>
-      product.name.toLowerCase().includes(value.toLowerCase())
-    );
-
-    setValue("");
-    setAllItens(false);
-    setFilteredItems(filteredItemsSearch);
-  };
-
   const clickPageProduct = (id) => {
     const selected = items.find((product) => product.id === id);
-    setIsHome(false);
-    setIsPageFavoritos(false);
-    setIsPageProduct(true);
     setProductExib(selected);
     setFilteredItems(selected);
     setValue("");
   };
-
-  const [categoryItem, setCategoryItem] = useState(null);
+  /*/
+  }
 
   return (
     <div className="w-full flex justify-center items-center bg-[#F5F5F5] overflow-x-hidden">
-      {isHome && (
-        <div>
+      <div>
+        <HomeContextProvider>
           <div className="w-[393px] min-h-screen flex flex-col px-[33px] pt-[37px] pb-20">
             <Header />
-            <div className="flex pt-[44px]">
-              <div className="w-[263px] h-[60px] flex items-center justify-center bg-[#fff] rounded-[30px]">
-                <div className="w-[210px] h-[50px] flex items-center">
-                  <div>
-                    <Search className="text-blackOpacity " size={26} />
-                  </div>
-                  <div>
-                    <input
-                      type="text"
-                      placeholder="Pesquise"
-                      value={value}
-                      className="w-[180px] h-[50px] text-blackOpacity outline-none pr-[16px] pl-3"
-                      onChange={(e) => setValue(e.target.value)}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="pl-5">
-                <button
-                  className="w-[54px] h-[54px] flex items-center justify-center text- bg-redPrimary rounded-full"
-                  onClick={handleClickFilterItem}
-                >
-                  <SlidersHorizontal className="text-white" />
-                </button>
-              </div>
-            </div>
+            <SearchItens />
 
-            <FilterItensContext.Provider
-              value={{
-                items,
-                clickPageProduct,
-                isSelected,
-                setCategoryItem,
-                setIsSelected,
-                setAllItens,
-                setFilteredItems,
-                arrayFavoritos,
-                setArrayFavoritos,
-              }}
-            >
-              <Buttons setCategoryItem={setCategoryItem} />
-              <FilterCategory categoryItem={categoryItem} />
-            </FilterItensContext.Provider>
-
-            {allItens && (
-              <TodosItens
-                itemsBest={itemsBest}
-                clickPageProduct={clickPageProduct}
-                arrayFavoritos={arrayFavoritos}
-                setArrayFavoritos={setArrayFavoritos}
-                items={items}
-              />
-            )}
-
+            <Buttons
+              setAllItens={setAllItens}
+              setFilteredItems={setFilteredItems}
+            />
+            <FilterCategory />
+            {allItens && <TodosItens />}
             {filteredItems && (
-              <FilterItemsValue
-                clickPageProduct={clickPageProduct}
-                arrayFavoritos={arrayFavoritos}
-                setArrayFavoritos={setArrayFavoritos}
-                items={items}
-                filteredItems={filteredItems}
-              />
+              <FilterItemsValue filteredItems={filteredItems} />
             )}
           </div>
-          <Menu
-            home={setIsHome}
-            pageCarrinho={setIsPageCarrinho}
-            pagePedido={setIsPagePedido}
-            favoritosPage={setIsPageFavoritos}
-            arrayProductCarrinho={arrayProductCarrinho}
-          />
-        </div>
-      )}
+          <Menu />
+        </HomeContextProvider>
+      </div>
 
+      {/*
       {isPageProduct && (
         <ProductPage
           setIsHome={setIsHome}
@@ -209,7 +121,7 @@ export default function Home() {
           pagePedido={setIsPagePedido}
           pedidosRealizados={arrayPedidosRealizados}
         />
-      )}
+      )} */}
     </div>
   );
 }
