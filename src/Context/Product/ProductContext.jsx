74e2bdbea60fb8f8e2cc47388aff03cd278e4b6c
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-11 14:20:50 -0300

    change ProductContext

--- src/Context/Product/ProductContext.jsx
+++ src/Context/Product/ProductContext.jsx
@@ -2,12 +2,15 @@
 
 import { createContext, useState } from "react";
 
-export const ProductContext = createContext({ countItem: 1 });
+export const ProductContext = createContext();
 
 export default function ProductContextProvider({ children }) {
   const [countItem, setCountItem] = useState(1);
-  const [arrayCarrinho, setArrayCarrinho] = useState([])
+  const [arrayCarrinho, setArrayCarrinho] = useState([]);
   const [valuePayment, setValuePayment] = useState("0");
+
+  const [arrayPedidos, setArrayPedidos] = useState([]);
+  const [arrayPedidosRealizados, setArrayPedidosRealizados] = useState([]);
 
   const handleClickAddItem = () => {
     setCountItem((prevNumber) => prevNumber + 1);
@@ -22,9 +25,44 @@ export default function ProductContextProvider({ children }) {
   };
 
 
+  const [modalPedido, setModalPedido] = useState(false);
+  const [pagamentoTrue, setPagamentotrue] = useState(false);
+  const handleClickAddPedido = () => {
+    if (valuePayment === "0") {
+      setPagamentotrue(true);
+      alert("Selecione uma forma de pagamento");
+      return;
+    }
+
+    // Adiciona o valor de pagamento ao objeto do carrinho antes de adicionar ao array de pedidos
+    const carrinhoComPagamento = arrayCarrinho.map((item) => ({
+      ...item,
+      payment: valuePayment,
+    }));
+
+    setArrayPedidos([...arrayPedidosRealizados, carrinhoComPagamento]);
+    setArrayPedidosRealizados([
+      ...arrayPedidosRealizados,
+      carrinhoComPagamento,
+    ]);
+
+    setModalPedido(true);
+  };
+
   return (
     <ProductContext.Provider
-      value={{ countItem, handleClickAddItem, handleClickMinusItem, arrayCarrinho, setArrayCarrinho, valuePayment }}
+      value={{
+        countItem,
+        handleClickAddItem,
+        handleClickMinusItem,
+        arrayCarrinho,
+        setArrayCarrinho,
+        valuePayment,
+        setValuePayment,
+        modalPedido, 
+        pagamentoTrue,
+        handleClickAddPedido
+      }}
     >
       {children}
     </ProductContext.Provider>
