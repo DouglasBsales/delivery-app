447e93f5ba8061abea5179c6cac7f594420fb495
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-11 14:20:19 -0300

    change component Menu with link for page Carrinho

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -2,6 +2,7 @@ import { Home, ShoppingCart, Heart, ClipboardList } from "lucide-react";
 import Link from "next/link";
 
 const Menu = () => {
+  
   return (
     <div className="fixed bottom-0">
       <div className="w-[393px] h-[65px] flex items-center justify-around bg-redPrimary rounded-t-md pt-2">
@@ -9,7 +10,7 @@ const Menu = () => {
           <Home className="text-white" size={30} />
         </Link>
         <div className="flex flex-col relative">
-          <button>
+          <Link href="/Carrinho">
             <div className="absolute top-[-12px] left-5 ">
               <p className=" text-white text-xs">
                 {/*{arrayProductCarrinho.reduce(
@@ -19,7 +20,7 @@ const Menu = () => {
               </p>
             </div>
             <ShoppingCart className="text-white" size={30} />
-          </button>
+          </Link>
         </div>
         <button>
           <Heart className="text-white" size={30} />
