1cfee4d18892af83355ef2536d6f62250ed8c8b5
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-11 22:24:34 -0300

    change component TodosItens with loading logic

--- src/components/Home/TodosItens.jsx
+++ src/components/Home/TodosItens.jsx
@@ -2,38 +2,43 @@ import { useContext } from "react";
 import { HomeContext } from "@/Context/Home/HomeContext";
 
 import CardsProducts from "./CardsProducts";
+import Loading from "./Loading";
 
 const TodosItens = () => {
-  const { itemsBest, items } = useContext(HomeContext);
+  const { itemsBest, items, loading } = useContext(HomeContext);
 
   return (
     <div>
-      <div>
-        <div className="pt-[36px]">
-          <p className="font-semibold text-[18px] text-blackPrimary">
-            Mais procurados
-          </p>
-        </div>
-        {itemsBest &&
-          itemsBest.map((item) => (
-            <div key={item.id}>
-              <CardsProducts item={{ ...item }} />
-            </div>
-          ))}
+      {loading ? (
+        <Loading />
+      ) : (
         <div>
           <div className="pt-[36px]">
             <p className="font-semibold text-[18px] text-blackPrimary">
-              Demais itens
+              Mais procurados
             </p>
           </div>
-          {items &&
-            items.map((item) => (
+          {itemsBest &&
+            itemsBest.map((item) => (
               <div key={item.id}>
                 <CardsProducts item={{ ...item }} />
               </div>
             ))}
+          <div>
+            <div className="pt-[36px]">
+              <p className="font-semibold text-[18px] text-blackPrimary">
+                Demais itens
+              </p>
+            </div>
+            {items &&
+              items.map((item) => (
+                <div key={item.id}>
+                  <CardsProducts item={{ ...item }} />
+                </div>
+              ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
