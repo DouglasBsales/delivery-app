438c64c0a43c3a5cd269777c89ceb1e2ba4444a2
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-12 15:34:38 -0300

    change menu with link and styles

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -1,8 +1,15 @@
-import { Home, ShoppingCart, Heart, ClipboardList } from "lucide-react";
+"use client";
+
 import Link from "next/link";
 
+import { useContext } from "react";
+import { ProductContext } from "@/Context/Product/ProductContext";
+
+import { Home, ShoppingCart, Heart, ClipboardList } from "lucide-react";
+
 const Menu = () => {
-  
+  const { arrayCarrinho } = useContext(ProductContext);
+
   return (
     <div className="fixed bottom-0">
       <div className="w-[393px] h-[65px] flex items-center justify-around bg-redPrimary rounded-t-md pt-2">
@@ -11,20 +18,22 @@ const Menu = () => {
         </Link>
         <div className="flex flex-col relative">
           <Link href="/Carrinho">
-            <div className="absolute top-[-12px] left-5 ">
-              <p className=" text-white text-xs">
-                {/*{arrayProductCarrinho.reduce(
-                (total, produto) => total + produto.quantity,
-                0
-              )} */}
-              </p>
+            <div className="absolute top-[-10px] left-4 ">
+              <div className="w-[25px] h-[25px] flex justify-center itens-end  bg-white rounded-full">
+                <p className="text-redPrimary font-semibold">
+                  {arrayCarrinho.reduce(
+                    (total, produto) => total + produto.quantity,
+                    0
+                  )}
+                </p>
+              </div>
             </div>
             <ShoppingCart className="text-white" size={30} />
           </Link>
         </div>
-        <button>
+        <Link href="/Favoritos">
           <Heart className="text-white" size={30} />
-        </button>
+        </Link>
         <Link href="/Pedidos">
           <ClipboardList className="text-white" size={30} />
         </Link>
