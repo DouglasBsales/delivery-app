32833ad8189150b1c6d3acd8c4ba059955ab2784
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-12 21:40:56 -0300

    added logic and classes in visual of menu indication user location

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -2,25 +2,63 @@
 
 import Link from "next/link";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductContext } from "@/Context/Product/ProductContext";
 
 import { Home, ShoppingCart, Heart, ClipboardList } from "lucide-react";
 
 const Menu = () => {
   const { arrayCarrinho } = useContext(ProductContext);
+  const [isPageSelected, setIsPageSelected] = useState("Home");
 
   return (
     <div className="fixed bottom-0">
-      <div className="w-[393px] h-[65px] flex items-center justify-around bg-redPrimary rounded-t-md pt-2">
-        <Link href="/Home">
-          <Home className="text-white" size={30} />
+      <div className="w-[393px] h-[65px] flex items-center justify-around bg-redPrimary rounded-t-md ">
+        <Link
+          href="/Home"
+          className={`${
+            isPageSelected === "Home"
+              ? "bg-white p-3 rounded-md"
+              : "bg-redPrimary"
+          } ease-in duration-200`}
+          onClick={() => setIsPageSelected("Home")}
+        >
+          <Home
+            size={30}
+            className={`${
+              isPageSelected === "Home" ? "text-redPrimary" : "text-white"
+            }  ease-in duration-200`}
+          />
         </Link>
         <div className="flex flex-col relative">
-          <Link href="/Carrinho">
-            <div className="absolute top-[-10px] left-4 ">
-              <div className=" w-[25px] h-[25px] flex justify-center itens-end  bg-white rounded-full">
-                <p className="text-redPrimary font-semibold relative top-[1px]">
+          <Link
+            href="/Carrinho"
+            className={`${
+              isPageSelected === "Carrinho"
+                ? "bg-white p-3 pr-4 rounded-md"
+                : "bg-redPrimary"
+            }  ease-in duration-200`}
+            onClick={() => setIsPageSelected("Carrinho")}
+          >
+            <div
+              className={`absolute ${
+                isPageSelected === "Carrinho"
+                  ? "top-[2px] left-7"
+                  : "top-[-10px] left-4"
+              } ease-in duration-200`}
+            >
+              <div
+                className={`w-[25px] h-[25px] flex justify-center itens-end rounded-full ${
+                  isPageSelected === "Carrinho" ? "bg-redPrimary" : "bg-white "
+                } ease-in duration-200`}
+              >
+                <p
+                  className={`font-semibold relative top-[1px] ${
+                    isPageSelected === "Carrinho"
+                      ? "text-white"
+                      : "text-redPrimary "
+                  } ease-in duration-200`}
+                >
                   {arrayCarrinho.reduce(
                     (total, produto) => total + produto.quantity,
                     0
@@ -28,14 +66,45 @@ const Menu = () => {
                 </p>
               </div>
             </div>
-            <ShoppingCart className="text-white" size={30} />
+            <ShoppingCart
+              className={`${
+                isPageSelected === "Carrinho" ? "text-redPrimary" : "text-white"
+              } ease-in duration-200`}
+              size={30}
+            />
           </Link>
         </div>
-        <Link href="/Favoritos">
-          <Heart className="text-white" size={30} />
+        <Link
+          href="/Favoritos"
+          className={`${
+            isPageSelected === "Favoritos"
+              ? "bg-white p-3 rounded-md"
+              : "bg-redPrimary"
+          }  ease-in duration-100 `}
+          onClick={() => setIsPageSelected("Favoritos")}
+        >
+          <Heart
+            className={`${
+              isPageSelected === "Favoritos" ? "text-redPrimary" : "text-white"
+            }  ease-in duration-100 `}
+            size={30}
+          />
         </Link>
-        <Link href="/Pedidos">
-          <ClipboardList className="text-white" size={30} />
+        <Link
+          href="/Pedidos"
+          className={`${
+            isPageSelected === "Pedidos"
+              ? "bg-white p-3 rounded-md"
+              : "bg-redPrimary"
+          } ease-in duration-200`}
+          onClick={() => setIsPageSelected("Pedidos")}
+        >
+          <ClipboardList
+            className={`${
+              isPageSelected === "Pedidos" ? "text-redPrimary" : "text-white"
+            } ease-in duration-200`}
+            size={30}
+          />
         </Link>
       </div>
     </div>
