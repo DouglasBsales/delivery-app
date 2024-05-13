e683ee8b5cc44e3a4d12fe8cc86a7f3b805fc0b2
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-13 11:53:31 -0300

    refactor: change padding-top

--- src/components/Pedidos/PedidoVazio.jsx
+++ src/components/Pedidos/PedidoVazio.jsx
@@ -4,7 +4,7 @@ import pedidoNaoRealizado from "../../assets/PedidoNaoRealizado.png";
 const PedidoVazio = () => {
   return (
     <div className="flex flex-col mx-auto">
-      <div className="flex justify-center pt-[120px] ">
+      <div className="flex justify-center pt-[50px] ">
         <Image src={pedidoNaoRealizado} alt="imagem" width={250} height={250} />
       </div>
       <div className="w-[270px] flex flex-col items-center justify-center pt-5">
