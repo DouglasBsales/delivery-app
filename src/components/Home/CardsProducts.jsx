4083a05d578ad6b68814443629eb80af4419915a
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-05-15 10:30:07 -0300

    refactor: change useEffect

--- src/components/Home/CardsProducts.jsx
+++ src/components/Home/CardsProducts.jsx
@@ -1,96 +1,67 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import { useState, useEffect, useContext } from "react";
 import { HomeContext } from "@/Context/Home/HomeContext";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { ProductContext } from "@/Context/Product/ProductContext";
 
 const CardsProducts = ({ item }) => {
-  const { arrayFavoritos, setArrayFavoritos, items, setProductExib, setValue } =
-    useContext(HomeContext);
+  const { arrayFavoritos, setArrayFavoritos, items, setProductExib, setValue } = useContext(HomeContext);
   const { setCountItem } = useContext(ProductContext);
-
   const [isFavorit, setIsFavorit] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      if (localStorage.getItem("favoritos") !== null) {
-        localStorage.setItem("favoritos", JSON.stringify(arrayFavoritos));
+      const savedFavoritos = window.localStorage.getItem("favoritos");
+      if (savedFavoritos) {
+        setArrayFavoritos(JSON.parse(savedFavoritos));
       }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem("favoritos", JSON.stringify(arrayFavoritos));
     }
   }, [arrayFavoritos]);
 
   useEffect(() => {
-   if(typeof window !== "undefined"){
-    localStorage.getItem(
-      "favoritos",
-      JSON.parse(localStorage.getItem("favoritos"))
-    );
-   }
-  }, []);
+    if (arrayFavoritos && arrayFavoritos.length > 0) {
+      const isProductInFavoritos = arrayFavoritos.some(product => product.id === item.id);
+      setIsFavorit(isProductInFavoritos);
+    }
+  }, [arrayFavoritos, item.id]);
 
   const exibirProduct = (id) => {
-    const selected = items.find((product) => product.id === id);
+    const selected = items.find(product => product.id === id);
     setValue("");
     setProductExib(selected);
     setCountItem(1);
   };
 
-  // Verifica se o produto atual está nos favoritos ao carregar a página
-  useEffect(() => {
-    if (arrayFavoritos && arrayFavoritos.length > 0) {
-      const itensFavoritos = JSON.parse(localStorage.getItem("favoritos"));
-
-      const isProductInFavoritos = itensFavoritos.some(
-        (product) => product.id === item.id
-      );
-      setIsFavorit(isProductInFavoritos);
-    }
-  }, [arrayFavoritos, item.id]);
-
   const toggleFavoritos = (id) => {
-    setIsFavorit(!isFavorit);
+    const isProductInFavoritos = arrayFavoritos.some(product => product.id === id);
 
-    if (arrayFavoritos && arrayFavoritos.length > 0) {
-      const isProductInFavoritos = arrayFavoritos.some(
-        (product) => product.id === id
-      );
-
-      if (isProductInFavoritos) {
-        const updatedFavoritos = arrayFavoritos.filter(
-          (product) => product.id !== id
-        );
-        setArrayFavoritos(updatedFavoritos);
-        return;
-      }
+    if (isProductInFavoritos) {
+      const updatedFavoritos = arrayFavoritos.filter(product => product.id !== id);
+      setArrayFavoritos(updatedFavoritos);
+    } else {
+      const selected = items.find(product => product.id === id);
+      setArrayFavoritos([...arrayFavoritos, selected]);
     }
-
-    const selected = items.find((product) => product.id === id);
-    setArrayFavoritos([...arrayFavoritos, selected]);
   };
 
   return (
     <div className="flex flex-col pt-8">
       <div>
         <div className="w-[315px] justify-end flex absolute pt-[10px]">
-          <button
-            onClick={() => toggleFavoritos(item.id)}
-            className="cursor-pointer"
-          >
+          <button onClick={() => toggleFavoritos(item.id)} className="cursor-pointer">
             {isFavorit ? (
-              <FontAwesomeIcon
-                icon={solidHeart}
-                className="text-redPrimary size-6"
-              />
+              <FontAwesomeIcon icon={solidHeart} className="text-redPrimary size-6" />
             ) : (
-              <FontAwesomeIcon
-                icon={regularHeart}
-                className="text-redPrimary size-6"
-              />
+              <FontAwesomeIcon icon={regularHeart} className="text-redPrimary size-6" />
             )}
           </button>
         </div>
@@ -101,18 +72,14 @@ const CardsProducts = ({ item }) => {
         >
           <div className="flex justify-between">
             <div className="w-[160px] pt-[21px] text-start">
-              <p className="text-xs text-blackOpacity font-semibold">
-                {item.category}
-              </p>
+              <p className="text-xs text-blackOpacity font-semibold">{item.category}</p>
               <p className="text-[18px] text-blackPrimary font-semibold relative top-[-3px]">
                 {item.name}
               </p>
               <p className="text-redPrimary text-xs font-semibold relative bottom-2">
                 {item.portion}
               </p>
-              <p className="text-redPrimary text-xs font-semibold relative bottom-2">
-                {item.ml}
-              </p>
+              <p className="text-redPrimary text-xs font-semibold relative bottom-2">{item.ml}</p>
               <p className="text-[10px] font-semibold text-blackOpacity">
                 {item.descriptionCard}...
               </p>
